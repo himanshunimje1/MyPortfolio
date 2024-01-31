de46f23b7faf3ce5eb657261c218ab7db0f05b14
Himanshu Nimje personel <himanshunimje@live.com>
2024-01-31 15:42:22 +0100

    portfolioskills done complete

--- force-app/main/default/lwc/portfolioSkills/portfolioSkills.js
+++ force-app/main/default/lwc/portfolioSkills/portfolioSkills.js
@@ -5,6 +5,11 @@ import SOFT_SKILLS_FIELD from '@salesforce/schema/Portfolio__c.SoftSkills__c'
 import SOFTWARE_FIELD from '@salesforce/schema/Portfolio__c.SoftwareTools__c'
 import METHODOLOGIES_FIELD from '@salesforce/schema/Portfolio__c.SoftwareDevelopmentMethodologies__c'
 export default class PortfolioSkills extends LightningElement {
+    techSkills = []
+    softSkills =[]
+    methoddologies=[]
+    toolsSkills = []
+
     @api recordId
     @wire(getRecord, {
         recordId:'$recordId',
@@ -12,10 +17,19 @@ export default class PortfolioSkills extends LightningElement {
     })skillHandler({data, error}){
         if(data){
             console.log("Skills Data", JSON.stringify(data))
+            this.formatSkills(data)
         }
         if(error){
             console.error("Skills error", error)
         }
+    }
+
+    formatSkills(data){
+        const {SoftSkills__c, SoftwareDevelopmentMethodologies__c, SoftwareTools__c,TechnicalSkills__c} = data.fields
+        this.techSkills = TechnicalSkills__c ? TechnicalSkills__c.value.split(','):[]
+        this.softSkills = SoftSkills__c ? SoftSkills__c.value.split(','):[]
+        this.methoddologies = SoftwareDevelopmentMethodologies__c ? SoftwareDevelopmentMethodologies__c.value.split(','):[]
+        this.toolsSkills = SoftwareTools__c ? SoftwareTools__c.value.split(','):[]
     }
 
 }
